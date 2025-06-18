f659db909a848ff14b692f4c335ef61496618316
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-19 00:09:16 +0600

    feat: add CRUD operations for mango items in the controller and routes

--- src/modules/mango/mango.controller.ts
+++ src/modules/mango/mango.controller.ts
@@ -29,7 +29,62 @@ const getMangos = async (req: Request, res: Response) => {
     });
   } catch (error) {
     res.status(500).json({
-      message: "Failed to retrieve users",
+      message: "Failed to retrieve mango",
+      error,
+    });
+  }
+};
+
+const getMangoById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const mangos = await Mango.findById(id);
+    res.status(201).json({
+      status: "success",
+      message: "Mango retrieved by Id  successfully",
+      data: mangos,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to retrieve by id mango",
+      error,
+    });
+  }
+};
+
+const updateMangoById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const payload = req.body;
+    const mangos = await Mango.findByIdAndUpdate(id, payload, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(201).json({
+      status: "success",
+      message: "Mango update by Id successfully",
+      data: mangos,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to update by id mango",
+      error,
+    });
+  }
+};
+
+const deleteMangoById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const mango = await Mango.findByIdAndDelete(id);
+    res.status(201).json({
+      status: "success",
+      message: "Mango delete by Id successfully",
+      data: mango,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to delete by id mango",
       error,
     });
   }
@@ -38,4 +93,7 @@ const getMangos = async (req: Request, res: Response) => {
 export const mangoController = {
   createMango,
   getMangos,
+  getMangoById,
+  updateMangoById,
+  deleteMangoById,
 };
--- src/modules/mango/mango.route.ts
+++ src/modules/mango/mango.route.ts
@@ -5,5 +5,8 @@ const mangoRoute = express.Router();
 
 mangoRoute.post("/", mangoController.createMango);
 mangoRoute.get("/", mangoController.getMangos);
+mangoRoute.get("/:id", mangoController.getMangoById);
+mangoRoute.patch("/:id", mangoController.updateMangoById);
+mangoRoute.delete("/:id", mangoController.deleteMangoById);
 
 export default mangoRoute;
