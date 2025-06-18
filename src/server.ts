4989fa627627bc67971f255d7bc85f34005375fe
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-18 16:06:01 +0600

    feat: add dotenv configuration and setup server with mongoose connection
    - Added dotenv for environment variable management.
    - Created a config module to manage application settings.
    - Updated server initialization to connect to MongoDB using mongoose.
    - Added express.json middleware for parsing JSON requests.
    - Introduced a development script for easier local development.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,25 @@
 import express from "express";
 import cors from "cors";
+import config from "./config";
+import mongoose from "mongoose";
 
 const app = express();
 
 //middleware
 app.use(cors());
+app.use(express.json());
+
+//server
+const server = async () => {
+  try {
+    await mongoose.connect(config.database_url!);
+    console.log("Database connected successfully");
+    app.listen(config.port, () => {
+      console.log(`Server is running on http://localhost:${config.port}`);
+    });
+  } catch (error) {
+    console.error("Database connection error:", error);
+    process.exit(1); // Exit the process with failure
+  }
+};
+server();
