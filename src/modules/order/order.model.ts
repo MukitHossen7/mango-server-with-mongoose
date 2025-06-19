563ae853da77b8312cf7845bdda268b49874a241
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-19 09:42:44 +0600

    refactor: improve formatting of address and order schemas for better readability

--- src/modules/order/order.model.ts
+++ src/modules/order/order.model.ts
@@ -1,56 +1,68 @@
 import { model, Schema } from "mongoose";
 import { IAddress, IOrder } from "./order.interface";
 
-const addressSchema = new Schema<IAddress>({
-  zipcode: {
-    type: String,
-    required: true,
-  },
-  state: {
-    type: String,
-    required: true,
-  },
-  country: {
-    type: String,
-    required: true,
-  },
-  street: {
-    type: String,
-    required: true,
-  },
-});
-
-const orderSchema = new Schema<IOrder>({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  mango: {
-    type: Schema.Types.ObjectId,
-    ref: "Mango",
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
-  totalPrice: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
-  status: {
-    type: String,
-    enum: {
-      values: ["pending", "process", "complete"],
-      message: "{VALUE} is not a valid status",
+const addressSchema = new Schema<IAddress>(
+  {
+    zipcode: {
+      type: String,
+      required: true,
     },
-    required: true,
+    state: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    street: {
+      type: String,
+      required: true,
+    },
   },
-  address: addressSchema,
-});
+  { _id: false }
+);
+
+const orderSchema = new Schema<IOrder>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    mango: {
+      type: Schema.Types.ObjectId,
+      ref: "Mango",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ["pending", "process", "complete"],
+        message: "{VALUE} is not a valid status",
+      },
+      required: true,
+    },
+    address: {
+      type: addressSchema,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
 const Order = model<IOrder>("Order", orderSchema);
 export default Order;
