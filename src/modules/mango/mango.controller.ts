0f80e9ef7d1cb1054c6e2fbe09b636df975c77f8
MukitHossen7 <hossenmukit7@gmail.com>
2025-07-14 16:56:38 +0600

    feat: implement catchAsync utility and refactor createMango to use it

--- src/modules/mango/mango.controller.ts
+++ src/modules/mango/mango.controller.ts
@@ -1,23 +1,17 @@
 import { Request, Response } from "express";
 import Mango from "./mango.model";
 import { mangoServices } from "./mango.service";
+import { catchAsync } from "../../utils/catchAsync";
 
-const createMango = async (req: Request, res: Response) => {
-  try {
-    const payload = req.body;
-    const savedMango = await mangoServices.createMangoIntoDB(payload);
-    res.status(201).json({
-      status: "success",
-      message: "Mango created successfully",
-      data: savedMango,
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: "Internal Server Error",
-      error,
-    });
-  }
-};
+const createMango = catchAsync(async (req: Request, res: Response) => {
+  const payload = req.body;
+  const savedMango = await mangoServices.createMangoIntoDB(payload);
+  res.status(201).json({
+    status: "success",
+    message: "Mango created successfully",
+    data: savedMango,
+  });
+});
 
 const getMangos = async (req: Request, res: Response) => {
   try {
