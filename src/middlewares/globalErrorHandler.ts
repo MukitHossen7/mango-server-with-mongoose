3f07c82c2b80a4addbaaa47b0c3e7cf314a443dd
MukitHossen7 <hossenmukit7@gmail.com>
2025-07-20 20:41:41 +0600

    modify gobal error handler

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import config from "../config";
 import AppError from "../error/AppError";
+import { any, ZodError } from "zod";
 
 export const globalErrorHandler = (
   error: any,
@@ -10,19 +11,43 @@ export const globalErrorHandler = (
 ) => {
   let statusCode = 500;
   let message = "Something Went Wrong!!";
+  let errorSources: any[] = [];
+
+  if (error.code === 11000) {
+    const duplicate = error.message.match(/"([^"]*)"/)[1];
+    message = `${duplicate} is already exists`;
+  } else if (error.name === "CastError") {
+    message = "Invalid Mongodb ObjectId";
+  } else if (error.name === "ValidationError") {
+    const errors = Object.values(error.errors);
+    errors.forEach((err: any) => {
+      errorSources.push({
+        path: err.path,
+        message: err.message,
+      });
+    });
+  } else if (error instanceof ZodError) {
+    error.issues.forEach((issue) => {
+      errorSources.push({
+        path: issue.path[issue.path.length - 1],
+        message: issue.message,
+      });
+    });
+  }
 
   if (error instanceof AppError) {
     statusCode = error.statusCode;
-    message = error.message;
+    message = message || error.message;
   } else if (error instanceof Error) {
     statusCode = 500;
-    message = error.message;
+    message = message || error.message;
   }
-
+  console.log(message);
   res.status(statusCode).json({
     success: false,
     message,
-    error,
+    errorDetails: error,
+    error: errorSources,
     stack: config.node_env === "development" ? error.stack : null,
   });
 };
